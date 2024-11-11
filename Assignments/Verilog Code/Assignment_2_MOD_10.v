19201035155657d477da25746cdaad93fda2b802
L Ramya <142811131+LankaRamya@users.noreply.github.com>
2024-11-11 20:10:00 +0530

    Create Assignment_2_MOD_10.v

---
+++ Assignments/Verilog Code/Assignment_2_MOD_10.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+module mod10_counter (
+    input clk,
+    input reset,
+    output reg [3:0] count
+);
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            count <= 0;
+        else if (count == 9)
+            count <= 0;
+        else
+            count <= count + 1;
+    end
+endmodule
+
